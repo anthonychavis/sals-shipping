9a573525f91509c88f230b9042ed40fe7007ba20
Anthony Chavis <gitanthony@yahoo.com>
2024-01-05 11:12:04 -0500

    exception handling - allow program to continue execution

--- use_the_fxn.py
+++ use_the_fxn.py
@@ -1,13 +1,13 @@
-from shipping_class import pkg
+# from shipping_class import pkg
 from low_shipping import find_lowest_shipping
 
-print(pkg)
+# print(pkg)
 # print(pkg.drone_shipping_cost)
 find_lowest_shipping()
 print()
-pkg.weight = 2
+# pkg.weight = 2
 # print('hmm')
-print('hey', pkg)
+# print('hey', pkg)
 # print(pkg.drone_shipping_cost)
 find_lowest_shipping(2)
 
@@ -18,28 +18,28 @@ find_lowest_shipping(2)
 # 
 
 print()
-pkg.weight = 1.5
-print(pkg)
+# pkg.weight = 1.5
+# print(pkg)
 find_lowest_shipping(1.5)
 
 print()
-pkg.weight = 4.8
-print(pkg)
+# pkg.weight = 4.8
+# print(pkg)
 find_lowest_shipping(4.8)
 
 print()
-pkg.weight = 41.5
-print(pkg)
+# pkg.weight = 41.5
+# print(pkg)
 find_lowest_shipping(41.5)
 
 print()
-pkg.weight = 8.07
-print(pkg)
+# pkg.weight = 8.07
+# print(pkg)
 find_lowest_shipping(8.07)
 
 print()
-pkg.weight = -0
-print(pkg)
+# pkg.weight = -0
+# print(pkg)
 find_lowest_shipping(-0)
 
 print()
@@ -52,8 +52,8 @@ print()
 # pkg.weight = [0]
 # print(pkg)
 # find_lowest_shipping([0])
-pkg.weight = (0)
-print(pkg)
+# pkg.weight = (0)
+# print(pkg)
 find_lowest_shipping((0))
 # find_lowest_shipping((2))
 # find_lowest_shipping((1.5))
@@ -65,11 +65,11 @@ print()
 
 print()
 # pkg.weight = ('zero')
-# find_lowest_shipping(('zero'))
+find_lowest_shipping(('zero'))
 
 # print()
-pkg.weight = 'zero'
-print(pkg)
+# pkg.weight = 'zero'
+# print(pkg)
 find_lowest_shipping('zero')
 # find_lowest_shipping(2)
 
