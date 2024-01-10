3b1d67f2d92019dd927795902c927037f6c923c4
Anthony Chavis <gitanthony@yahoo.com>
2024-01-10 13:39:23 -0500

    Python type alias - take 1

--- low_shipping.py
+++ low_shipping.py
@@ -1,20 +1,24 @@
-from custom_exceptions import NegativeWeightError 
+from custom_exceptions import NegativeWeightError
+from typing import Dict, Tuple
+# from typing import Dict, Optional, Tuple, Union
 def find_lowest_shipping(weight: int | float = 0):
   '''inform customer of the lowest cost method of shipping their package based on its weight'''
   try:
     if not isinstance(weight, (int, float)): raise TypeError(f'You entered {weight}; only positive numerical values (int, float), or 0, are allowed.')
     if weight < 0: raise NegativeWeightError(weight)
-    ground = {
+    MultipliersTuple = Tuple[float, float, float, float]
+    ShippingDict = Dict[str, float | MultipliersTuple]  # check !!
+    ground:ShippingDict = {
       # 'shipping_type': 'Standard Ground Shipping',
       'shipping_cost': 20.,
       'multipliers': (1.5, 3., 4., 4.75)
     }
-    drone = {
+    drone: ShippingDict = {
       # 'shipping_type': 'Drone Shipping',
       'shipping_cost': 0.,
       'multipliers': (4.5, 9., 12., 14.25)
     }
-    def shipping_cost_fxn(weight: int | float, shipping_cost: float, multipliers: float):
+    def shipping_cost_fxn(weight: int | float, shipping_cost: float, multipliers: MultipliersTuple):
       """calculate shipping cost based on package weight when shipped via drone or standard ground"""
       if weight <=2: shipping_cost += weight * multipliers[0]
       elif weight <= 6: shipping_cost += weight * multipliers[1]
@@ -27,13 +31,13 @@ def find_lowest_shipping(weight: int | float = 0):
   except TypeError as e: print(f'💀 this is why on that day, Types were made 🐞\n{e.__class__.__name__}: {e}')
   except Exception as e: print(f'💀 there\'s a {e.__class__.__name__} 🐞\n{e}')
   else:
-    premium = {
+    premium: ShippingDict = {
       'shipping_type': 'Premium Ground Shipping',
       'shipping_cost': 125.
       }
-    
     conditions = (ground_shipping_cost, 'Ground Shipping') if ground_shipping_cost < drone_shipping_cost else (drone_shipping_cost, 'Drone Shipping')
-    
     print(f'The lowest cost method of shipping for a package that weighs {weight} lbs is %s at $%s' %(conditions[1] if conditions[0] < premium['shipping_cost'] else premium['shipping_type'], '{:.2f}'.format(conditions[0]) if conditions[0] < premium['shipping_cost'] else "{:.2f}".format(premium['shipping_cost'])))
     # print(locals())
   finally: print('🧁')
+
+  # static code analysis !!
