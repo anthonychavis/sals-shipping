d40279e7694aa5faa1856b58788f8024cfa325b8
Anthony Chavis <gitanthony@yahoo.com>
2024-01-13 02:16:52 -0500

    setUp pass

--- unittest_low_shipping.py
+++ unittest_low_shipping.py
@@ -1,6 +1,9 @@
 import unittest
 
 class LowShippingTests(unittest.TestCase):
+    def setUp(self) -> None:
+        # return super().setUp()
+        pass
     @unittest.expectedFailure
     def test_str_input(self):
         raise TypeError('This test will fail')
